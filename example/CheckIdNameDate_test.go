7ebf762a85164d506cbe80dad7018c90f78cd415
mojitozhang <mojitozhang@tencent.com>
2023-08-31 16:02:47 +0800

    demo优化

--- example/CheckIdNameDate_test.go
+++ example/CheckIdNameDate_test.go
@@ -2,6 +2,7 @@ package example
 
 import (
 	"encoding/json"
+	"github.com/tencentcloud/faceid-api-crypto-golang"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/errors"
 	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
--- faceid/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/tencentcloud/faceid-api-crypto-golang/faceid
-
-go 1.16
-
-require (
-	github.com/tjfoc/gmsm v1.4.1
-	golang.org/x/crypto v0.9.0 // indirect
-)
