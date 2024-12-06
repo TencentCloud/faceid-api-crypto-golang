94fa459f55cfea1333ad7c3260ee1e8d41ca0624
haoyhyuan <haoyhyuan@tencent.com>
2024-12-06 15:38:49 +0800

    GetDetectInfoEnhanced 支持包体加密

--- example/GetDetectInfoEnhanced_test.go
+++ example/GetDetectInfoEnhanced_test.go
@@ -20,26 +20,29 @@ func TestGetDetectInfoEnhanced(t *testing.T) {
 		SecretKey = ""            // TODO 腾讯云密钥
 	)
 
-	// Step 1. 生成对称密钥，用于加解密敏感信息
+	/// Step 1. 生成对称密钥，用于加解密敏感信息
 	key := faceid.GenerateKey(Algorithm)
 
-	// Step 2. 组装加密参数并对敏感数据加密
-	reqJson, err := faceid.Encrypt(Algorithm, key, nil)
+	// Step 2. 生成加密参数
+	reqJson, err := faceid.BodyEncrypt(Algorithm, key, "")
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Printf("req json:%s \n", reqJson)
+	log.Printf("ciphertext desc:%s", reqJson)
 
-	// Step 2. 使用Tencent Cloud API SDK组装请求体，填充参数
+	// Step 3. 使用Tencent Cloud API SDK组装请求体，填充参数
 	credential := common.NewCredential(SecretId, SecretKey)
 	client, _ := tencentcloudsdk.NewClient(credential, regions.Guangzhou, profile.NewClientProfile())
 	request := tencentcloudsdk.NewGetDetectInfoEnhancedRequest()
-	_ = json.Unmarshal([]byte(reqJson), request)
+	err = json.Unmarshal(reqJson, request)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	// Step 4. TODO 根据您的业务需要，设置其他参数，详情参考api文档：https://cloud.tencent.com/document/product/1007/41957
 	request.RuleId = common.StringPtr("")
-	request.InfoType = common.StringPtr("")
 	request.BizToken = common.StringPtr("")
+	request.IsEncryptResponse = common.BoolPtr(true)
 
 	// Step 5. 调用接口
 	response, err := client.GetDetectInfoEnhanced(request)
@@ -53,107 +56,14 @@ func TestGetDetectInfoEnhanced(t *testing.T) {
 	log.Printf("%s \n", response.ToJsonString())
 
 	// Step 6. 组装需要解密的参数
-	m := make(map[string]string)
-	m["Response.Text.IdCard"] = *response.Response.Text.IdCard
-	m["Response.Text.Name"] = *response.Response.Text.Name
-	m["Response.Text.OcrIdCard"] = *response.Response.Text.OcrIdCard
-	m["Response.Text.OcrName"] = *response.Response.Text.OcrName
-	m["Response.Text.LivenessDetail[0].Idcard"] = *response.Response.Text.LivenessDetail[0].Idcard
-	m["Response.Text.LivenessDetail[0].Name"] = *response.Response.Text.LivenessDetail[0].Name
-	tagList := common.StringValues(response.Response.Encryption.TagList)
-	iv := *response.Response.Encryption.Iv
-
-	// Step 7. 解密接敏感信息
-	resultMap, err := faceid.Decrypt(Algorithm, key, iv, tagList, m)
+	// Step 6. 解密接敏感信息
+	var tagList []string
+	for _, s := range response.Response.Encryption.TagList {
+		tagList = append(tagList, *s)
+	}
+	plaintext, err := faceid.BodyDecrypt(Algorithm, key, *response.Response.Encryption.Iv, tagList, *response.Response.EncryptedBody)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	for k, v := range resultMap {
-		log.Printf("key:%s value:%s \n", k, v)
-	}
-
+	log.Printf("plaintext response: %s", string(plaintext))
 }
-
-// Response 示例
-// {
-//    "Response": {
-//        "Text": {
-//            "ErrCode": 2006,
-//            "ErrMsg": "姓名和身份证号不匹配，请核实身份后重试",
-//            "IdCard": "wueSQRrp69xBMjJxXX5eCfcP",
-//            "UseIDType": 0,
-//            "Name": "EWkAlJJR",
-//            "OcrNation": null,
-//            "OcrAddress": null,
-//            "OcrBirth": null,
-//            "OcrAuthority": null,
-//            "OcrValidDate": null,
-//            "OcrName": "EWkAlJJR",
-//            "OcrIdCard": "wueSQRrp69xBMjJxXX5eCfcP",
-//            "OcrGender": null,
-//            "IdInfoFrom": "手动输入",
-//            "LiveStatus": 0,
-//            "LiveMsg": "成功",
-//            "Comparestatus": 2006,
-//            "Comparemsg": "姓名和身份证号不匹配，请核实身份后重试",
-//            "CompareLibType": "权威库",
-//            "LivenessMode": 3,
-//            "Sim": "0.00",
-//            "Location": null,
-//            "Mobile": "",
-//            "NFCRequestIds": [],
-//            "NFCBillingCounts": 0,
-//            "Extra": "",
-//            "LivenessDetail": [
-//                {
-//                    "ReqTime": "1692272463607",
-//                    "Seq": "6a45d7d6-2c3a-4ebb-8977-f17571f5844c",
-//                    "Idcard": "wueSQRrp69xBMjJxXX5eCfcP",
-//                    "Name": "EWkAlJJR",
-//                    "Sim": "0.00",
-//                    "IsNeedCharge": true,
-//                    "Errcode": 2006,
-//                    "Errmsg": "姓名和身份证号不匹配，请核实身份后重试",
-//                    "Livestatus": 0,
-//                    "Livemsg": "成功",
-//                    "Comparestatus": 2006,
-//                    "Comparemsg": "姓名和身份证号不匹配，请核实身份后重试",
-//                    "CompareLibType": "权威库",
-//                    "LivenessMode": 3
-//                }
-//            ],
-//            "PassNo": null,
-//            "VisaNum": null
-//        },
-//        "IdCardData": null,
-//        "BestFrame": null,
-//        "VideoData": null,
-//        "IsCustomizeStorage": false,
-//        "BucketName": "",
-//        "IntentionVerifyData": null,
-//        "IntentionQuestionResult": null,
-//        "IntentionActionResult": null,
-//        "RequestId": "e907d58c-2069-48a4-beb6-691fb3eafcfd",
-//        "Encryption": {
-//            "Algorithm": "SM4-GCM",
-//            "CiphertextBlob": "BJr625A7qLxvp784g2A+rXN5OTcp9uoL0LLShyfQi6TtwDUkfqJEw6+cwepexe0IjwsQYSlXYeY221Ao0OaSU16DjQ/xj8PcL08XcSFVUk2w4rb4jpEedQ7JVoxP98AwExFA2PqzU/lG9M6nkSiz1d0=",
-//            "EncryptList": [
-//                "Response.Text.IdCard",
-//                "Response.Text.Name",
-//                "Response.Text.OcrIdCard",
-//                "Response.Text.OcrName",
-//                "Response.Text.LivenessDetail[0].Idcard",
-//                "Response.Text.LivenessDetail[0].Name"
-//            ],
-//            "TagList": [
-//                "rIZ3ATfhaKWgo5Yxiuz0xw==",
-//                "ODTnlmCGm1253czvBm+lYQ==",
-//                "rIZ3ATfhaKWgo5Yxiuz0xw==",
-//                "ODTnlmCGm1253czvBm+lYQ==",
-//                "rIZ3ATfhaKWgo5Yxiuz0xw==",
-//                "ODTnlmCGm1253czvBm+lYQ=="
-//            ],
-//            "Iv": "vP5I2L8GVqav5Da8"
-//        }
-//    }
-//}
